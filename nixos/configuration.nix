635dd3d3cba720a24d7d4d0b0b98ebedd2351044
dolphounet <maxencepj.04@gmail.com>
2024-07-31 19:18:35 +0200

    added fcitx5

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -117,6 +117,17 @@ in {
     })
   ];
 
+  # fcitx5
+  i18n.inputMethod = {
+   type = "fcitx5";
+   enable = true;
+   fcitx5.waylandFrontend = true;
+   fcitx5.addons = with pkgs; [
+     fcitx5-mozc
+     fcitx5-gtk
+     fcitx5-tokyonight
+   ];
+  };
   # Home manager
   home-manager = {
     useUserPackages = true;
