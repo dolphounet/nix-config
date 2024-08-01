5fa011c17c17ea4855a153e255d1bda98fc15516
dolphounet <maxencepj.04@gmail.com>
2024-08-01 14:27:01 +0200

    removed unnecessary packages

--- home-manager/packages.nix
+++ home-manager/packages.nix
@@ -1,13 +1,11 @@
 {pkgs, inputs, ...}: {
   home.packages = with pkgs; [
-    librewolf
     bat
     cava
     hyprpaper
     libreoffice-fresh
     hyprcursor
     chezmoi
-    wofi
     gnome.gnome-themes-extra
     gnome.gnome-calendar
     nwg-look
