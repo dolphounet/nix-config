7ce32629ea78bfaeaae3a28d342b67bb181b07ee
dolphounet <maxencepj.04@gmail.com>
2024-08-01 12:22:51 +0200

    added hyprland and hyprlock

---
+++ home-manager/hyprland.nix
@@ -0,0 +1,159 @@
+{
+  inputs,
+  pkgs,
+  ...
+}: {
+  wayland.windowManager.hyprland = {
+    enable = true;
+    xwayland.enable = true;
+    settings = {
+    exec-once = [
+        "wl-paste --watch cliphist store"
+        "fcitx5"
+        "hypridle"
+        "hyprpaper"
+        "ags"
+        "hyprctl setcursor Catppuccin-Mocha-Lavender-Cursors 32"
+      ];
+      xwayland = {
+        force_zero_scaling = true;
+      };
+
+      input = {
+        kb_layout = "fr";
+        follow_mouse = 1;
+        numlock_by_default = true;
+        touchpad = {
+          natural_scroll = "yes";
+        };
+      };
+
+      general = {
+        gaps_in = 5;
+        gaps_out = 20;
+        border_size = 0;
+        "col.active_border" = "rgba(33ccffee) rgba(00ff99ee) 45deg";
+        "col.inactive_border" = "rgba(595959aa)";
+        layout = "dwindle";
+      };
+
+      decoration = {
+        rounding = 4;
+        blur = {
+          enabled = true;
+          popups = true;
+          size = 3;
+          passes = 1;
+        };
+        drop_shadow = "yes";
+        shadow_range = 4;
+        shadow_render_power = 3;
+        "col.shadow" = "rgba(1a1a1aee)";
+      };
+
+      animations = {
+        enable = "yes";
+        bezier = [
+          "myBezier, 0.05, 0.9, 0.1, 1.05"
+          "anim, 0, 1, 0.5, 0.85"
+        ];
+        animation = [
+          "window, 1, 4, anim"
+          "windowsOut, 1, 1, myBezier, popin 80%"
+          "windowsMove, 1, 5, anim"
+          "border, 1, 10, myBezier"
+          "borderangle, 1, 8, myBezier"
+          "fade, 1, 4, default"
+          "workspaces, 1, 6, default"
+        ];
+      };
+
+      dwindle = {
+        pseudotile = "yes";
+        preserve_split = "yes";
+      };
+
+      gestures = {
+        workspace_swipe = "off";
+      };
+
+      windowsrule = [
+        "tile, ^(MATLAB R2023b - academic use)$"
+      ];
+
+      windowrulev2 = [
+        "tile,class:^(MATLAB R2023b - academic use)$,title:^(MATLAB R2023b - academic use)$"
+        "float, title:^(Picture-in-Picture)$"
+        "tile, class:^(de-neemann-digital-gui-Main)$, title:^(Digital)$"
+      ];
+
+      layerrule = [
+        "blur, ^(.*)$"
+        "ignorezero, ^(.*)$"
+      ];
+
+      misc = {
+        disable_hyprland_logo = true;
+        vrr = 1;
+        enable_swallow = true;
+        swallow_regex = "^(foot)$";
+        animate_manual_resizes = true;
+        animate_mouse_windowdragging = true;
+      };
+
+      "$mainMod" = "SUPER";
+      bind = [
+        "$mainMod, Q, exec, foot"
+        "$mainMod, C, killactive,"
+        "$mainMod, E, exec, pcmanfm"
+        "$mainMod, f, togglefloating,"
+        "$mainMod, P, pseudo,"
+        ", XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
+        "$mainMod SHIFT, s, exec, hyprctl keyword monitor ,preferred,auto,1 && IMG = ~/Images/$(date +%Y-%m-%d_%H-%M-%S).png && grim -g '$(slurp)' $IMG && wl-copy < $IMG && hyprctl keyword monitor ,preferred,auto,1.5"
+        "$mainMod, x, exec, ags -t powermenu"
+        "$mainMod, v, exec, cliphist list | wofi --show dmenu | cliphist decode | wl-copy"
+        "$mainMod, left, movefocus, l"
+        "$mainMod, right, movefocus, r"
+        "$mainMod, up, movefocus, u"
+        "$mainMod, down, movefocus, d"
+        "$mainMod, ampersand, workspace, 1"
+        "$mainMod, eacute, workspace, 2"
+        "$mainMod, quotedbl, workspace, 3"
+        "$mainMod, apostrophe, workspace, 4"
+        "$mainMod, parenleft, workspace, 5"
+        "$mainMod, minus, workspace, 6"
+        "$mainMod, egrave, workspace, 7"
+        "$mainMod, underscore, workspace, 8"
+        "$mainMod, ccedilla, workspace, 9"
+        "$mainMod, agrave, workspace, 10"
+
+        "$mainMod SHIFT, ampersand, movetoworkspace, 1"
+        "$mainMod SHIFT, eacute, movetoworkspace, 2"
+        "$mainMod SHIFT, quotedbl, movetoworkspace, 3"
+        "$mainMod SHIFT, apostrophe, movetoworkspace, 4"
+        "$mainMod SHIFT, parenleft, movetoworkspace, 5"
+        "$mainMod SHIFT, minus, movetoworkspace, 6"
+        "$mainMod SHIFT, egrave, movetoworkspace, 7"
+        "$mainMod SHIFT, underscore, movetoworkspace, 8"
+        "$mainMod SHIFT, ccedilla, movetoworkspace, 9"
+        "$mainMod SHIFT, agrave, movetoworkspace, 10"
+        ", F11, fullscreen"
+      ];
+      binde = [
+        ", XF86MonBrightnessDown, exec, brightnessctl set 5%-"
+        ", XF86MonBrightnessUp, exec, brightnessctl set +5%"
+        ", XF86AudioRaiseVolume, exec, wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 5%+"
+        ", XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"
+        ", Next, exec, brightnessctl -d asus::kbd_backlight set 1-"
+        ", Prior, exec, brightnessctl -d asus::kbd_backlight set +1"
+      ];
+      bindm = [
+        "$mainMod, mouse:272, movewindow"
+        "$mainMod, mouse:273, resizewindow"
+      ];
+      bindri = [
+        "$mainMod, SUPER_L, exec, pkill wofi || wofi --show drun"
+      ]
+    };
+  };
+}
---
+++ home-manager/hyprlock.nix
@@ -0,0 +1,74 @@
+{
+  inputs,
+  pkgs,
+  ...
+}: {
+  security.pam.services.hyprlock = {};
+  programs.hyprlock = {
+    enable = true;
+    settings = {
+      general = {
+        hide_cursor = true;
+      };
+
+      label = [{
+        monitor = "";
+        text = "CA GAZPAR HAHAHA";
+        color = "rgba(200, 200, 200, 1.0)";
+        font_size = 25;
+        font_family = "JetBrainsMono Nerd Font";
+        position = "0, 150";
+        halign = "center";
+        valign = "left";
+      }
+      {
+        monitor = "";
+        text = "Hi there, $USER";
+        color = "rgba(200, 200, 200, 1.0)";
+        font_size = 40;
+        font_family = "JetBrainsMono Nerd Font";
+        position = "0, 150";
+        halign = "center";
+        valign = "center";
+      }
+      ];
+
+      input-field = [ {
+        monitor = "";
+        size = "200, 200";
+        outline_thickness = 2;
+        dots_size = 0.33; # Scale of input-field height, 0.2 - 0.8
+        dots_spacing = 0.15; # Scale of dots' absolute size, 0.0 - 1.0
+        dots_center = false;
+        outer_color = "rgb(30, 30, 46)";
+        inner_color = "rgb(180, 190, 245)";
+        font_color = "rgb(30, 30, 46)";
+        fade_on_empty = true;
+        fade_timeout = 1000;
+        placeholder_text = "<i>Input Password...</i>"; # Text rendered in the input box when it's empty.
+        hide_input = true;
+
+        position = "0, -20";
+        halign = "center";
+        valign = "center";
+      }
+      ];
+
+      background = [ {
+        monitor = "";
+        # path = screenshot   # only png supported for now
+        color = "rgba(30, 30, 46, 0.5)";
+
+        # all these options are taken from hyprland, see https://wiki.hyprland.org/Configuring/Variables/#blur for explanations
+        blur_passes = 1; # 0 disables blurring
+        blur_size = 1;
+        noise = 0.0117;
+        contrast = 0.8916;
+        brightness = 0.8172;
+        vibrancy = 0.1696;
+        vibrancy_darkness = 0.0;
+      }
+      ];
+    };
+  };
+}
