702e505d335bba715b85d21fa58e5448c8b71be6
dolphounet <maxencepj.04@gmail.com>
2024-12-30 00:25:03 +0100

    ghostty config file

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -1,25 +1,7 @@
 # This is your home-manager configuration file
 # Use this to configure your home environment (it replaces ~/.config/nixpkgs/home.nix)
-{
-  inputs,
-  lib,
-  config,
-  pkgs,
-  ...
-}: {
+{...}: {
   nixpkgs = {
-    # You can add overlays here
-    overlays = [
-      # If you want to use overlays exported from other flakes:
-      # neovim-nightly-overlay.overlays.default
-
-      # Or define it inline, for example:
-      # (final: prev: {
-      #   hi = final.hello.overrideAttrs (oldAttrs: {
-      #     patches = [ ./change-hello-to-hi.patch ];
-      #   });
-      # })
-    ];
     # Configure your nixpkgs instance
     config = {
       # Disable if you don't want unfree packages
@@ -41,6 +23,12 @@
 
   # Enable home-manager
   programs.home-manager.enable = true;
+
+  # Ghostty config file
+  home.file.".config/ghostty/config" = {
+    source = ../ghostty/config;
+    recursive = true;
+  };
 
   # Nicely reload system units when changing configs
   systemd.user.startServices = "sd-switch";
